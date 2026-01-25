5788f2f3ecd484f688d04ccbfc04af6f68a73005
Sarvajnya Pujari <pujarsarvajnya5@gmail.com>
2026-01-25 21:41:51 +0530

    latest

---
+++ 2193D.py
@@ -0,0 +1,184 @@
+
+def main():
+    t = si()
+    output_list = []
+    for _ in range(t):
+        n=si() 
+        a=li() 
+        b=li() 
+        req=0 
+        a.sort()
+        pref=[0]*(n+1)
+        for i in range(n):
+            pref[i+1]=pref[i]+b[i]
+        d=defaultdict(int)
+        for i in range(n):
+            d[Wrapper(a[i])]=max(d[Wrapper(a[i])], n-bisect_left(a, a[i]))
+        # print(d.values())
+        ans=-inf
+        # print(pref)
+        pref=pref[1:]
+        for k,v in d.items():
+            x=Wrapper(k)
+            temp=v 
+            i=0 
+            temp2=0
+            # print(x,temp,sep=' ')
+            ind=bisect_right(pref, temp)
+            # print(ind)
+            # while i<n:
+            #     if temp-b[i]>=0:
+            #         temp-=b[i] 
+            #         temp2+=1 
+            #     else:
+            #         break
+            #     # print(temp, i, sep=' ')
+            #     i+=1
+                
+            ans=max(ans,ind*x)
+            # print(ans, x, temp, i, temp2, v,sep=' ')
+        output_list+=[ans]
+
+
+        
+    print('\n'.join(map(str, output_list)).strip())
+    pass
+
+import os
+import sys
+from io import BytesIO, IOBase
+
+import random
+import os
+
+from bisect import *
+from typing import *
+from collections import *
+from copy import *
+from functools import *
+from heapq import *
+from itertools import *
+from string import *
+from math import *
+mod = 1e9+7
+def input(): return sys.stdin.readline().strip()
+
+
+BUFsiZE = 4096
+
+
+#Fast IO using PyRival
+
+RANDOM = random.randrange(2**62)
+
+
+def Wrapper(x):
+  return x ^ RANDOM
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(
+                os.fstat(self._fd).st_size, BUFsiZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(
+                b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(
+                os.fstat(self._fd).st_size, BUFsiZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(
+                b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdout = IOWrapper(sys.stdout)
+
+
+def print(*args, end='\n', sep=''):
+    for i in args:
+        sys.stdout.write(str(i))
+        sys.stdout.write(sep)
+    sys.stdout.write(end)
+
+
+def si(types=None):
+    if not types:
+        return int(input().strip())
+    return int(types)
+
+
+def sf(types=None):
+    if not types:
+        return float(input().strip())
+    return float(types)
+
+
+def ss(types=None):
+    if not types:
+        return list(input().strip())
+    return list(str(types))
+
+
+def li(types=None):
+    if not types:
+        return list(map(int, input().strip().split()))
+    return list(map(int, str(types)))
+
+
+def mi(types):
+    return map(int, str(types))
+
+
+def ms(types):
+    return map(str, str(types))
+
+
+def mf(types):
+    return map(float, str(types))
+
+
+def lf(types=None):
+    if not types:
+        return list(map(float, input().strip().split()))
+    return list(map(float, str(types)))
+
+
+def ls(types=None):
+    if not types:
+        return list(input().strip().split())
+    return list(map(str, str(types)))
+
+if __name__ == '__main__':
+    main()
