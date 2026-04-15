1fa531c2a72beada0b229a49b2337d4cb97c88f2
Sarvajnya Pujari <pujarsarvajnya5@gmail.com>
2026-04-15 23:30:19 +0530

    CC

--- bruteforce.py
+++ bruteforce.py
@@ -2,23 +2,25 @@
 # b = [4, 5, 8, 2, 8, 4, 3, 5, 2, 3]
 # n = len(a)
 # from template import *
-with open('debug.txt', 'w') as f:
+# with open('debug.txt', 'w') as f:
 #     for i in range(1, 500001):
 #         f.write(f"{i}\n")
     
-        
+import math
 # # print(True)
     
-    s=set()
-    for i in range(1, 10001):
-        o,e=0,0
+    # s=set()
+    # for i in range(1, 10001):
+    #     o,e=0,0
         
-        for j in range(1, i+1):
-            if i%j == 0:
-                if j&1:
-                    o+=1 
-                else:
-                    e+=1 
-        if e%o == 0:
-            f.write(f"{o}\n")
-            s.add(o)
+    #     for j in range(1, i+1):
+    #         if i%j == 0:
+    #             if j&1:
+    #                 o+=1 
+    #             else:
+    #                 e+=1 
+    #     if e%o == 0:
+    #         f.write(f"{o}\n")
+    #         s.add(o)
+val=4/5
+print(math.asin(val) * 180/math.pi )
